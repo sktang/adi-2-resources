138f978c6388ef90a70a31acfe3383de49209b4a
Kitty Tang <Kittys.Tang@gmail.com>
2016-06-15 12:41:30 -0700

    practice

---
+++ week-2-intro-android/08-wednesday-activities/lessons/activites-and-intents-lesson/Practice/Activity_Practice/app/src/main/java/com/example/kitty/activity_practice/EchoActivity.java
@@ -0,0 +1,9 @@
+package com.example.kitty.activity_practice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class EchoActivity extends AppCompatActivity {R.id.)
+    }
+}
